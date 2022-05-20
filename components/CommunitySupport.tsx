fc4af14af4977aa7b987e628e438b25c3b9f9721
Lantt <laanfor@gmail.com>
2022-05-20 12:14:01 +0800

    fix: feedback

--- components/CommunitySupport.tsx
+++ components/CommunitySupport.tsx
@@ -54,55 +54,55 @@ const config = [
     name: 'TinTin',
     image:
       '/images/community-support/tintin.png 1x, /images/community-support/tintin@2x.png 2x, /images/community-support/tintin@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: 'https://twitter.com/Tintinland2021',
   },
   {
     name: 'Genki',
     image:
       '/images/community-support/genki.png 1x, /images/community-support/genki@2x.png 2x, /images/community-support/genki@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: 'SeeDao',
     image:
       '/images/community-support/seedao.png 1x, /images/community-support/seedao@2x.png 2x, /images/community-support/seedao@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: 'ShowMe',
     image:
       '/images/community-support/showme.png 1x, /images/community-support/showme@2x.png 2x, /images/community-support/showme@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: 'DeepLearning',
     image:
       '/images/community-support/deep_learning.png 1x, /images/community-support/deep_learning@2x.png 2x, /images/community-support/deep_learning@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: '427318916',
     image:
       '/images/community-support/427318916.png 1x, /images/community-support/427318916@2x.png 2x, /images/community-support/427318916@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: 'rebase',
     image:
       '/images/community-support/rebase.png 1x, /images/community-support/rebase@2x.png 2x, /images/community-support/rebase@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: 'Atem',
     image:
       '/images/community-support/atem.png 1x, /images/community-support/atem@2x.png 2x, /images/community-support/atem@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
   {
     name: 'Web3 Revolution',
     image:
       '/images/community-support/web3.png 1x, /images/community-support/web3@2x.png 2x, /images/community-support/web3@3x.png 3x',
-    link: 'https://moledao.io/#/home',
+    link: null,
   },
 ]
 
@@ -117,8 +117,8 @@ export const CommunitySupport = () => {
         <div className="grid grid-cols-3 grid-rows-3 gap-4 xl:grid-cols-5 xl:grid-rows-2">
           {config.map((x) => (
             <a
-              href={x.link}
-              target="_blank"
+              href={x.link ?? undefined}
+              target={x.link ? '_blank' : undefined}
               key={x.name}
               className="inline-flex justify-center rounded-lg bg-support px-2 py-3 xl:px-7 xl:py-8">
               {x.image.includes('.svg') ? <img src={x.image} alt={x.name} /> : <img srcSet={x.image} alt={x.name} />}
